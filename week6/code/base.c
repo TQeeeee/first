d601888d64175c22edf5b5a51f9824b16f9c684d
Ytq <798327052@qq.com>
2019-10-22 00:10:42 -0700

    week6 staticlib commit

---
+++ week6/code/base.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int Show(int*a,int n){
+	int i=0;
+	for(i=0;i<n;i++){
+		printf("%d\t",a[i]);
+	}
+}
---
+++ week6/code/test.c
@@ -0,0 +1,7 @@
+#include"mylib.h"
+#include<stdio.h>
+int main(){
+	int a[5]={1,2,3,4,5};
+	printf("max is %d\n",Max(a,5));
+	Show(a,5);
+}
